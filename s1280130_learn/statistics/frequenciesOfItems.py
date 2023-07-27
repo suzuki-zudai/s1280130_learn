83af26e928eae5ba8a7180621085ce21561f9e24
s1280130 suzuki <86813637+suzuki-zudai@users.noreply.github.com>
2023-07-27 15:50:27 +0900

    Update frequenciesOfItems.py

--- s1280130_learn/statistics/frequenciesOfItems.py
+++ s1280130_learn/statistics/frequenciesOfItems.py
@@ -1,29 +1,29 @@
 import pandas as pd
-class frequenciesOfItems(inputFile,sep="\t"):
+class frequenciesOfItems():
         """
             Storing the complete transactions of the database/input file in a database variable
         """
-        def __init__():
+        def __init__(self,inputFile,sep="\t"):
                 self.iFile=inputFile
                 self.sep=sep
-                data=self.countFre()
-                return data
-        def countFre():
-        """
-            Storing the complete transactions of the database/input file in a database variable
-        """
-        Database = {}
-        with open(self.iFile, 'r', encoding='utf-8') as f:
-              for line in f:
-                    line.strip()
-                    temp = [i.rstrip() for i in line.split(self.sep))]
-                    temp = [x for x in temp if x]
-                    for i in temp:
-                      if i not in Database.keys():
-                        Database[i]=1
-                      else:
-                        Datbase[i]+=1
-        return Database
+                
+        def countFre(self):
+            """
+                Storing the complete transactions of the database/input file in a database variable
+            """
+            Database = {}
+            with open(self.iFile, 'r', encoding='utf-8') as f:
+                  for line in f:
+                        line.strip()
+                        temp = [i.rstrip() for i in line.split(self.sep)]
+                        temp = [x for x in temp if x]
+                        for i in temp:
+                          if i not in Database.keys():
+                            Database[i]=1
+                          else:
+                            Datbase[i]+=1
+            return Database
 
 if __name__=="__main__":
-        x=frequenciesOfItems("inputdata","\t")
+        x=frequenciesOfItems("PM24HeavyPollutionRecordingSensors.csv","\t")
+        data=x.countFre()
