71a9860434e512f23451d61602401bd0b089e775
s1280130 suzuki <86813637+suzuki-zudai@users.noreply.github.com>
2023-07-27 15:06:04 +0900

    Update frequenciesOfItems.py

--- s1280130_learn/statistics/frequenciesOfItems.py
+++ s1280130_learn/statistics/frequenciesOfItems.py
@@ -1,15 +1,22 @@
 import pandas as pd
-def frequenciesOfItems(inputFile,sep):
+class frequenciesOfItems(inputFile,sep="\t"):
         """
             Storing the complete transactions of the database/input file in a database variable
-
-
+        """
+        def __init__():
+                self.iFile=inputFile
+                self.sep=sep
+                data=self.countFre()
+                return data
+        def countFre():
+        """
+            Storing the complete transactions of the database/input file in a database variable
         """
         Database = {}
-        with open(inputFilee, 'r', encoding='utf-8') as f:
+        with open(self.iFile, 'r', encoding='utf-8') as f:
               for line in f:
                     line.strip()
-                    temp = [i.rstrip() for i in line.splitsep)]
+                    temp = [i.rstrip() for i in line.split(self.sep))]
                     temp = [x for x in temp if x]
                     for i in temp:
                       if i not in Database.keys():
@@ -17,3 +24,6 @@ def frequenciesOfItems(inputFile,sep):
                       else:
                         Datbase[i]+=1
         return Database
+
+if __name__=="__main__":
+        x=frequenciesOfItems("inputdata","\t")
